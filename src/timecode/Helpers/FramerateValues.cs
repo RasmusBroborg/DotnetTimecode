b1d5e872415338203e86e44ae790d0a7756a62d9
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-09-25 20:27:11 +0200

    Create constructors. Implement constructor tests.

--- src/timecode/Helpers/FramerateValues.cs
+++ src/timecode/Helpers/FramerateValues.cs
@@ -6,19 +6,19 @@ namespace timecode.Helpers
   {
     internal static readonly Dictionary<Framerate, decimal> FramerateAsDecimals
     = new Dictionary<Framerate, decimal>
-{
-    { Framerate.fps23_976, 23.976m },
-    { Framerate.fps24, 24m },
-    { Framerate.fps25, 25m },
-    { Framerate.fps29_97_DF, 29.97m },
-    { Framerate.fps29_97_NDF, 29.97m },
-    { Framerate.fps30, 30m },
-    { Framerate.fps47_95, 47.95m },
-    { Framerate.fps48, 48m },
-    { Framerate.fps50, 50m },
-    { Framerate.fps59_94_DF, 59.94m },
-    { Framerate.fps59_94_NDF, 59.94m },
-    { Framerate.fps60, 60m },
-};
+    {
+      { Framerate.fps23_976, 23.976m },
+      { Framerate.fps24, 24m },
+      { Framerate.fps25, 25m },
+      { Framerate.fps29_97_DF, 29.97m },
+      { Framerate.fps29_97_NDF, 29.97m },
+      { Framerate.fps30, 30m },
+      { Framerate.fps47_95, 47.95m },
+      { Framerate.fps48, 48m },
+      { Framerate.fps50, 50m },
+      { Framerate.fps59_94_DF, 59.94m },
+      { Framerate.fps59_94_NDF, 59.94m },
+      { Framerate.fps60, 60m },
+    };
   }
 }
